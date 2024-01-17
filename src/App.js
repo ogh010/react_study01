d22b7f07cdfc9fd20be5d81ba737159f9883521b
gahee <ogh010@naver.com>
2024-01-17 14:01:25 +0900

    3 state
    # state 소개, state 사용 , key

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import Subject from './components/Subject'
 class App extends Component {
   constructor(props){
     super(props);
+    // state값을 초기화 시킴
+    // 컴포넌트가 실행될때 render라는 함수 전에 실행
+    // 컴포넌트가 실행될 때 construcotr라는 함수가 있으면 초기화를 담당한다.
     this.state = {
       subject:{title:'web',sub:'world wide web'},
       contents:[
         {id:1,title:'html',desc:'html is hypertext markup language'},
         {id:2,title:'css',desc:'css is hypertext markup language'},
-        {id:3,title:'j',desc:'js is hypertext markup language'},
+        {id:3,title:'js',desc:'js is hypertext markup language'},
       ]
     }
   }
--- src/components/Toc.js
+++ src/components/Toc.js
@@ -1,20 +1,26 @@
+// react 의 component 클래스를 가져옴
 import { Component } from 'react';
+
+// toc 클래스를 정의, component 클래스를 확장
 class Toc extends Component {
-    
+    // render 메서드 : 컴포넌트 ui를 생성
     render(){
+        // 빈 배열 생성
         let lists = [];
+        // 부모 컴포넌트로부터 전달받은 데이터를 가져옴
         let data = this.props.data;
         let i = 0;
+
+        // 반복문을 통해 데이터를 기반으로 목록을 생성
         while(i<data.length){
-            lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            // 목록에 각 아이템을 추가, 각 항목은 a 태그를 포함
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
             i=i+1;
         }
         return(
         <nav>
             <ul>
-                <li><a href="1.html">html</a></li>
-                <li><a href="2.html">css</a></li>
-                <li><a href="3.html">js</a></li>
+                {lists}
             </ul>
         </nav>
         )
